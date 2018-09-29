4748936f34eb1da0aaabad1f77bba94bd61a4db8
David Sid Olofsson <david.m.o@telia.com>
2018-09-30 00:01:11 +0200

    Base execution working (with recursion), next up is background execution and pipes.

--- lsh.c
+++ lsh.c
@@ -22,11 +22,22 @@ n * If you want to add functions in a separate file
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "parse.h"
+// for threading
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+// for separating command from arguments
+#include <string.h>
+// for debugging
+#include <errno.h>
+
 
 /*
  * Function declarations
  */
-int RunCommand(int, Command *);
+int RunCommand(Command *);
+int RunPgm(Pgm *);
+
 void PrintCommand(int, Command *);
 void PrintPgm(Pgm *);
 void stripwhite(char *);
@@ -77,11 +88,15 @@ int main(void)
 	}
 	//if normal, execute
 	else {
+
+	  //TODO: add meaningful error codes
 	  add_history(line);
-	  //Execute
+	  //Parse
 	  ret = parse(line, &cmd);
-	  //todo, run command with opts
-	  ret = RunCommand(ret, &cmd);
+	  if (ret != 1){ return 1; }
+	  //execute
+	  ret = RunCommand(&cmd);
+	  if (ret != 0){ return 1; }
 	  //Print debug information.
 	  PrintCommand(ret, &cmd);
 	}
@@ -99,26 +114,77 @@ int main(void)
  * Name: RunCommand
  *
  * Description: Executes the parsed command structure
+ * Returns error if any part of execution fails
  *
  */
 int
-RunCommand(int ret, Command *cmd)
+RunCommand(Command *cmd)
 {
+  int cpid;
+  int cret;
   printf("starting execution\n");
-  for(;;){
-    if( cmd->rstdin ){
-      printf("there is an rstdin set\n");
+  if( cmd->rstdin ){
+    printf("there is an rstdin set\n");
+  }
+  //figure out how to read from specified file
+  if( cmd->rstdout ){
+    printf("there is an rstdout set\n");
+    //if file address starts with / it is global, else appended to $PWD
+    puts("test");
+  }
+  //figure out how to write to specified file
+  if( cmd->bakground ){
+    printf("it should be run in background\n");
+  }
+  //write the branching
+  cpid = fork();
+  if (cpid == 0) {
+    //this thread is child
+    //run the program
+    RunPgm(cmd->pgm);
+    //Close the child process
+    exit(0);
+  }
+  else if (cpid > 0) {
+    //this thread is parent
+    //wait for child if background isn't set
+    waitpid(cpid, &cret, 0);
+    //TODO: add meaningful error codes
+    if(cret != 0){ return 1; }
+  }
+  else {
+    //error has occured
+    printf("errno is %d", errno);
+  }
+  return cret;
+}
+
+
+/*
+ * Name: RunPgm
+ *
+ * Description: Runs the pgm list recursively
+ * (I wish the parser had been readable and thus feasible to modify)
+ *
+ */
+int
+RunPgm (Pgm *p)
+{
+  int ret;
+  if (p == NULL) {
+    return 0;
+  }
+  else {
+    char **pl = p->pgmlist;
+    /* The list is in reversed order so print
+     * it reversed to get right
+     * (If it wasn't this could easily be done by looping...)
+     */
+    ret = RunPgm(p->next);
+    //if previous command worked, run next
+    if (ret == 0){
+      execvp(*pl, pl);
     }
-    //figure out how to read from specified file
-    if( cmd->rstdout ){
-      printf("there is an rstdout set\n");
-      //if file address starts with / it is global, else appended to $PWD
-    }
-    //figure out how to write to specified file
-    if( cmd->bakground ){
-      printf("it should be run in background\n");
-    }
-    //write the branching
     return ret;
   }
 }
